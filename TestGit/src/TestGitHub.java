b77fc6a5ae5fbefa7c7d6149a18ced2e0118a049
Ganya <Ganya@Ganya-PC>
2020-04-27 17:56:49 +0530

    ok

--- TestGit/src/TestGitHub.java
+++ TestGit/src/TestGitHub.java
@@ -3,7 +3,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Git Hub by Parag Davkhar");
+		System.out.println("Welcome to Git Hub by Parag Dav");
 	}
 
 }
